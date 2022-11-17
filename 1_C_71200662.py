c2b1a307d25afea9901835063150bc135745f609
Toms9999 <117893395+Toms9999@users.noreply.github.com>
2022-11-17 16:26:58 +0700

    Add files via upload

---
+++ 1_C_71200662.py
@@ -0,0 +1,7 @@
+masukkannamaanda = input (str ("Masukkan nama Anda :"))
+masukkanmatkul = input (str ("Masukkan nama Mata Kuiah:"))
+masukkangrup = input (str("Masukkan grup :"))
+    
+print("haloo!",masukkannamaanda)
+print(masukkannamaanda,"tergabung dalam kelas",masukkanmatkul,"pada",masukkangrup)
+
